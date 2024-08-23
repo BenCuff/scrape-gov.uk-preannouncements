64e4e09b74fd661af0eb9be2c862c484eb0195d0
BenCuff <113535359+BenCuff@users.noreply.github.com>
2024-08-23 11:08:24 +0100

    Add files via upload

---
+++ Scrape website.R
@@ -0,0 +1,124 @@
+library(tidyverse)
+library(rvest)
+library(lubridate)
+
+#Function to scrape HTML from the announcements page
+scrape <- function(){
+  
+  #Set up empty dataframe for the output
+  output <- data.frame(Title = as.character(),
+                       URL = as.character(),
+                       Type = as.character(),
+                       Organisation = as.character(),
+                       Date = as.character(),
+                       Time = as.character(),
+                       Status = as.character())
+  
+  #Function for cleaning up outputs
+  tidy_up <- function(df){
+    return(
+      df %>% 
+        str_replace(., "\n", "") %>% 
+        str_replace(., "Document type: |Organisation: |Release date: |State: ", "") %>% 
+        trimws()
+      )
+  }
+  
+  
+  i <- 1 #start the loop on page 1
+  results <- 0 #set up results value to monitor how many results are returned on each page
+  
+  
+  #This will loop through pages until zero results are returned (i.e., the end of the list) 
+  repeat{
+    
+    #Set URL of page to scrape (i is used for page number)
+    page <- read_html(paste0("https://www.gov.uk/search/research-and-statistics?content_store_document_type=upcoming_statistics&order=release-date-oldest&organisations%5B%5D=uk-health-security-agency&page=", 
+                            i))
+    
+    #Check number of items to see if a result will be returned
+    items <- html_nodes(page, '.gem-c-document-list__item')
+    results <- length(unlist(items))
+    if(results == 0){
+      break #stop the loop
+    }
+    
+    
+    #Get titles
+    titles <- page %>% html_nodes(".gem-c-document-list__item-title a") %>% html_text() %>% tidy_up()
+    
+    #Get URLs
+    urls <- page %>% html_nodes(".gem-c-document-list__item-title a") %>% html_attr("href") %>% tidy_up() %>% paste0("https://www.gov.uk", .)
+    
+    #Get descriptions
+    descriptions <- page %>% html_nodes(".gem-c-document-list__item p") %>% html_text() %>% tidy_up()
+    
+    #Get types
+    types <- page %>% html_nodes(".gem-c-document-list__item ul li:nth-child(1)") %>% html_text() %>% tidy_up()
+    
+    #Get orgs
+    orgs <- page %>% html_nodes(".gem-c-document-list__item ul li:nth-child(2)") %>% html_text() %>% tidy_up()
+    
+    #Get dates
+    dates <- page %>% html_nodes(".gem-c-document-list__item ul li:nth-child(3)") %>% html_text() %>% tidy_up() %>% str_replace_all(., " \\d{1,2}:\\d{2}[a|p]m$", "") 
+
+    #Get times
+    times <- page %>% html_nodes(".gem-c-document-list__item ul li:nth-child(3)") %>% html_text() %>% tidy_up() %>% str_replace_all(., ".* ", "")
+    
+    #Get statuses
+    statuses <- page %>% html_nodes(".gem-c-document-list__item ul li:nth-child(4)") %>% html_text() %>% tidy_up()
+    
+    
+    #Bind outputs for this page into the master list
+    output <- rbind(output,
+                    data.frame(Title = titles,
+                               URL = urls,
+                               Type = types,
+                               Organisation = orgs,
+                               Date = dates,
+                               Time = times,
+                               Status = statuses))
+    
+    
+    #Print confirmation message and move to next page
+    print(paste("Done page", i))
+    i <- i+1 
+    
+  }
+  
+  #Return completed output from all pages
+  return(output)
+
+}
+
+
+
+
+#Create list
+list <- scrape()
+
+
+#If you wish to filter by date:
+today <- Sys.Date()
+date_to_include <- today+days(7) #set number of days in future you want to include
+
+Filtered_list <- list %>% 
+                 mutate(Date = dmy(Date)) %>%  
+                 #You will get messages here that some have failed to parse. 
+                 #That's due to future provisional dates not being in a standard date format.
+                 #Shouldn't be an issue as everything should be confirmed within 28 days of release.
+                 filter(Date <= date_to_include)
+
+ 
+#Save csv output
+write.csv(list, paste0("release_calendar_", Sys.Date(), ".csv"), row.names = F)
+
+
+#Save markdown output
+text <- ""
+
+for(i in 1:nrow(list)){
+  text <- paste(text, "|", list[i, 'Date'], "|", list[i, 'Title'], "|", "\n")
+}
+
+cat(text, file="markdown.txt")
